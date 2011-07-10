telyn/node-jrc

commit da858483e4fb0b139c71a9eb371d5ebdd9bc0021

Refactor room joining, improve private message support.

lib/server.js
@@ -85,6 +85,44 @@ function Server(bind_address) {
         }
     };
 
+    this.moveUserToRoom(name,room) {
+        var prevroom = nametoroom[name];
+        var client = clientsbyname[name];
+
+        if(client === undefined) {
+            throw errors.UserNotFoundError("User not found",{user:name});
+        }
+
+        if(rooms[room] === undefined && room !== "") {
+            throw errors.RoomNotFoundError("Room not found",{room:room});
+        }
+
+        if(prevroom == room) {
+            return;
+        }
+
+        if(room !== "") {
+            rooms[room].push(client);
+            for(i=0; i<rooms[room].length; i++) {
+                rooms[room][i].receiveJoinedRoom(name,room);
+            }
+            nametoroom[client.name] = room;
+        } else {
+            delete nametoroom[client.name];
+        }
+
+        client.room = room;
+
+        if(prevroom !== null && prevroom !== undefined) {
+            if(rooms[prevroom] !== undefined) {
+                rooms[prevroom].splice(rooms[prevroom].indexOf(client),1);
+                for(i=0; i<rooms[prevroom].length; i++) {
+                    rooms[prevroom][i].receiveLeftRoom(client.name,prevroom);
+                }
+            }
+        }
+    };
+
     // **************************
     // Below this all are actions
     // **************************
@@ -169,35 +207,11 @@ function Server(bind_address) {
 
 
     this.joinRoom = function(client,room) {
+        //detect null rooms: don't allow users to null themselves
         if(room === null || room === "" || room === undefined) {
             throw errors.NullRoomError("Room is null.");
-        } else if(rooms[room] === undefined) {
-            throw errors.RoomNotFoundError("Room not found",{room:room});
         } else {
-            var prevroom = nametoroom[client.name];
-            
-            if(prevroom == room) {
-                return;
-            }
-
-            rooms[room].push(client);
-            client.room = room;
-            nametoroom[client.name] = room;
-            
-
-            for(i=0; i<rooms[room].length; i++) {
-                rooms[room][i].receiveJoinedRoom(client.name,room);
-            }
-
-
-            if(prevroom !== null && prevroom !== undefined) {
-                if(rooms[prevroom] !== undefined) {
-                    rooms[prevroom].splice(rooms[prevroom].indexOf(client),1);
-                    for(i=0; i<rooms[prevroom].length; i++) {
-                        rooms[prevroom][i].receiveLeftRoom(client.name,room);
-                    }
-                }
-            }
+            this.moveUserToRoom(client.name, room);
         }
     };
     
@@ -213,11 +227,17 @@ function Server(bind_address) {
     };
 
     this.sendPrivateMessage = function(from,destinations,message) {
+        var notfounds = [];
         for(var i=0; i<destinations.length; i++) {
             if(clientsbyname[destinations[i]] !== undefined) {
                 clientsbyname[from].sentPrivateMessage(destinations[i],message);
                 clientsbyname[destinations[i]].receivePrivateMessage(from,message);
+            } else {
+                notfounds.push(destinations[i]);
             }
+        }
+        if(notfounds.length > 0) {
+            throw errors.UsersNotFoundError("Users not found.",{users:notfounds});
         }
     };
 }

commit 4ff05ba5806862ddf803755c923e4bf4b1ff3763

Fixed a bug introduced in commit da85848 - bad function definition

lib/server.js
@@ -85,7 +85,7 @@ function Server(bind_address) {
         }
     };
 
-    this.moveUserToRoom(name,room) {
+    this.moveUserToRoom = function(name,room) {
         var prevroom = nametoroom[name];
         var client = clientsbyname[name];
 