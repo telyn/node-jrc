3b0596bd6d9309a6463ab8941df3b78a4325f86a
Norn Albion <norn@nornalbion.com>
2011-07-11 00:04:27 +0100

    Added banning support, currently masters-only. Also added first part of state support.

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 var jrc = require("./lib/server.js");
 var fs = require("fs");
 
-var config = JSON.parse(fs.readFileSync("./config.json"));
+var config = {};
+var state = {};
+var server = null;
 
-server = jrc.createServer("127.0.0.1",config);
-server.listen(41528);
+fs.readFile("./config.json", function(err,data) {
+    if(!err) {
+        config = JSON.parse(data);
+    }
+    fs.readFile("./state.json", function(err,data) {
+        if(!err) {
+            state = JSON.parse(data);
+        }
+        //god this is ugly.
+        server = jrc.createServer("127.0.0.1",config,state);
+        server.listen(41528);
+
+    });
+});
+
+var exit = function() {
+    console.log("Saving state and exiting.");
+    var state = server.saveState();
+    fs.writeFileSync("./state.json",state);
+    console.log("Goodbye.");
+};
+
+process.on('SIGINT',function() {
+    process.exit();
+});
+
+process.on('exit', exit);
