9d029bfc8960f4ea71a75812eab237d283f6e708
Norn Albion <norn@nornalbion.com>
2012-05-22 22:49:06 +0100

    Added more tests against the server's client connection

--- test/server.js
+++ test/server.js
@@ -3,6 +3,7 @@ var gently = new (require('gently'))();
 var Message = require('../lib/message.js');
 var constants = require('../lib/constants.js');
 var Client = require('../lib/server/client.js');
+var errors = require('../lib/errors.js');
 
 var streamstub;
 
@@ -118,6 +119,146 @@ describe('Server', function() {
 
             var client = new Client({debug:false}, streamstub);
             client.receiveWhois("steve", {Online: '5s', Level: 'Te'});
+        });
+
+
+        it('shouldn\'t do anything on a NullRoomError', function() {
+            var wroteSomething = false;
+            var error = errors.NullRoomError();
+            streamstub.write = function(data) {
+                wroteSomething = true;
+                
+            };
+
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+            wroteSomething.should.be.false;
+        });
+
+
+        it('should handle a RoomNotFoundError', function(done) {
+            var error = errors.RoomNotFoundError();
+            error.room = "Creatures";
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "xyb"+error.room;
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+
+        });
+
+
+        it('should handle a UserNotFoundError', function(done) {
+            var error = errors.UserNotFoundError();
+            error.user = "NornAlbion";
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "xya"+error.user;
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+        });
+
+
+        it('should handle a UsersNotFoundError', function(done) {
+            var error = errors.UsersNotFoundError();
+            error.users = ["NornAlbion","GameFreak"];
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "xya"+error.users.join("\t");
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+        });
+
+
+        it('should handle a NameTakenError', function(done) {
+            var error = errors.NameTakenError();
+            error.nick = "NornAlbion";
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "wCName in use.";
+                // this is actually a SERVERMESSAGE LEAVE
+                // rather than an actual
+                // REQUESTFAILED ACCESSDENIED NAME
+                // or anything similar.
+                // Because that's what JRC did.
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+        });
+
+
+        it('should handle a PasswordRequiredError', function(done) {
+            var error = errors.PasswordRequiredError();
+            error.username = "NornAlbion";
+
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "wB"+error.username;
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
+        });
+
+
+        it('should handle a WrongPasswordError', function(done) {
+            var error = errors.WrongPasswordError();
+            error.username = "NornAlbion";
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "xza"+error.username;
+                
+
+                data.should.equal(expected);
+            });
+
+            var client = new Client({debug:false}, streamstub);
+
+            gently.expect(client, 'leave', function(reason) {
+                done();
+            });
+
+            
+            client.handleError(error);
+        });
+
+
+        it('should handle a AccessDeniedToBanError', function(done) {
+            var error = errors.AccessDeniedToBanError();
+            error.address = "10.0.0.1";
+            
+            gently.expect(streamstub,'write', function(data) {
+                data = chomp(data);
+                var expected = "xzL"+error.address;
+                
+
+                data.should.equal(expected);
+                done();
+            });
+            var client = new Client({debug:false}, streamstub);
+            client.handleError(error);
         });
 
     });
