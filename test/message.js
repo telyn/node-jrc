191a98f7fb09b0d78df12ad0739b5f83ffcf0798
Norn Albion <norn@nornalbion.com>
2012-05-22 19:37:24 +0100

    Added tests. Improved message parsing. Added Client.receiveMail to lib/server/client.js

--- test/message.js
+++ test/message.js
@@ -121,8 +121,6 @@ describe('Server-bound Messages',function() {
             message.subcommand.should.equal(constants.INFO);
             message.subsubcommand.should.equal(constants.ROOMUSERLIST);
             
-
-            
             message.params.length.should.equal(2); 
             message.params[0].should.equal('autowhois');
             
@@ -183,23 +181,80 @@ describe('Server-bound Messages',function() {
         /*
         it('should correctly parse JRC_PERMROOM');
         it('should correctly parse JRC_SETINFO');
+        */
     });
 
     describe('That Are Totally Optional', function() {
+        it('should correctly parse mail inbox requests', function() {
+            var message = new Message("T");
+            message.command.should.equal(constants.MAIL);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(0);
+        });
+
+
+        it('should correctly parse mail count requests', function() {
+            var message = new Message("GT");
+            message.command.should.equal(constants.NUMERICINFO);
+            message.subcommand.should.equal(constants.MAIL);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(0); 
+        });
+
+
+        it('should correctly parse mail sending requests', function() {
+            var message = new Message("TGameFreak\tHello!");
+            message.command.should.equal(constants.MAIL);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("GameFreak");
+            message.params[1].should.equal("Hello!");
+        })
+        /*
         it('should correctly parse JRC_SERVERMESSAGE');
 
         it('should correctly parse JRC_SHUTDOWN');
         it('should correctly parse JRC_NOIGNORE');
         it('should correctly parse JRC_PASSWORD');
-        it('should correctly parse JRC_MAIL');
-        it('should correctly parse JRC_WHOIS');*/
+        
+        it('should correctly parse JRC_WHOIS');
+        */
     });
 
     describe('That I Don\'t Know The Purpose Of', function() {
         // JRNet doesn't actually use these, but they exist!
-        it('should correctly parse requests for the user count'); // JRC_NUMERICINFO
-        it('should correctly parse requests for the room count');
-        it('should correctly parse requests for the user count for a room');
+        it('should correctly parse requests for the user count', function() {
+            var message = new Message("Ga");
+            message.command.should.equal(constants.NUMERICINFO);
+            message.subcommand.should.equal(constants.USERINFO);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(0);
+        }); 
+
+
+        it('should correctly parse requests for the room count', function() {
+            var message = new Message("Gb");
+            message.command.should.equal(constants.NUMERICINFO);
+            message.subcommand.should.equal(constants.ROOMINFO);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(0);
+        });
+        it('should correctly parse requests for the user count for a room', function() {
+            var message = new Message("GcCreatures");
+            message.command.should.equal(constants.NUMERICINFO);
+            message.subcommand.should.equal(constants.ROOMUSERLIST);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(1);
+            message.params[0].should.equal("Creatures");
+        });
         /*
         it('should correctly parse JRC_UNDO');
         it('should correctly parse JRC_LOG');
@@ -216,8 +271,162 @@ describe('Server-bound Messages',function() {
     });
 });
 
+
+// ***********************************************************
+// * Client-bound messages                                   *
+// ***********************************************************
+
 describe('Client-bound Messages', function() {
     describe('That Are Really Necessary For Communication', function() {
+        it('should correctly parse incoming room-messages', function() {
+            var message = new Message('ENornAlbion\tHello!');
+            message.command.should.equal(constants.MESSAGE);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("NornAlbion");
+            message.params[1].should.equal("Hello!");
+        });
+
+
+        it('should correctly parse incoming private messages', function() {
+            //from me to GameFreak
+            var message = new Message('FNornAlbion\tGameFreak\tHello GameFreak!');
+            message.command.should.equal(constants.PRIVATEMESSAGE);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(3);
+            message.params[0].should.equal("NornAlbion");
+            message.params[1].should.equal("GameFreak");
+            message.params[2].should.equal("Hello GameFreak!");
+
+            // From GameFreak to me!
+            var message = new Message('FGameFreak\tHello NornAlbion!');
+            message.command.should.equal(constants.PRIVATEMESSAGE);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("GameFreak");
+            message.params[1].should.equal("Hello NornAlbion!");
+        });
+
+
+        it('should correctly parse incoming "(some person) joined (some room)" messages',function() {
+            var message = new Message('DNornAlbion\tCreatures');
+            message.command.should.equal(constants.JOIN);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("NornAlbion");
+            message.params[1].should.equal("Creatures");
+        });
+
+
+        it('should correctly parse incoming "Someone left (some room) messages', function() {
+           var message = new Message('CNornAlbion\tCreatures');
+            message.command.should.equal(constants.LEAVE);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("NornAlbion");
+            message.params[1].should.equal("Creatures"); 
+        });
+
+
+        it('should correctly parse incoming user list', function() {
+            var message = new Message('H\taNornAlbion\tGameFreak');
+            message.command.should.equal(constants.GENERALINFO);
+            message.subcommand.should.equal(constants.USERINFO);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("NornAlbion");
+            message.params[1].should.equal("GameFreak");
+        });
+
+
+        it('should correctly parse incoming room list', function() {
+            var message = new Message('H\tbCreatures\tCreatures RPG');
+            message.command.should.equal(constants.GENERALINFO);
+            message.subcommand.should.equal(constants.ROOMINFO);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(2);
+            message.params[0].should.equal("Creatures");
+            message.params[1].should.equal("Creatures RPG");
+        });
+
+
+        it('should correctly parse incoming users-in-room list', function() {
+            var message = new Message('H\tcCreatures\tNornAlbion\tGameFreak');
+            message.command.should.equal(constants.GENERALINFO);
+            message.subcommand.should.equal(constants.ROOMUSERLIST);
+            should.not.exist(message.subsubcommand);
+            
+            message.params.length.should.equal(3);
+            message.params[0].should.equal("Creatures");
+            message.params[1].should.equal("NornAlbion");
+            message.params[2].should.equal("GameFreak");
+        });
+
+
+        it('should correctly parse incoming whois', function() {
+            //um, here's one I made earlier. This is known to work.
+            var message =  new Message("^\tautowhois\tIanornalbion\tRoom\2Creatures\3IP\002127.0.0.1\3Access\2Ma\3Client\2null\3Idle\0023m20s\3Login\0025m32s\3");
+            message.command.should.equal(constants.WHOIS);
+            message.subcommand.should.equal(constants.INFO);
+            message.subsubcommand.should.equal(constants.USERINFO);
+            
+            message.params.length.should.equal(3);
+            message.params[0].should.equal("autowhois");
+            message.params[1].should.equal("nornalbion");
+            message.params[2].Room.should.equal("Creatures");
+            message.params[2].IP.should.equal("127.0.0.1");
+            message.params[2].Access.should.equal("Ma");
+            message.params[2].Idle.should.equal("3m20s");
+            message.params[2].Login.should.equal("5m32s");
+        });
+            
+    });
+
+    describe('That Are Necessary For User Control', function() {
+
+    });
+
+    describe('That Are Totally Optional', function() {
+        it('should correctly parse incoming mail', function() {
+            // directed at one person
+            var message = new Message("TGameFreak\tNornAlbion\tHello!");
+            message.command.should.equal(constants.MAIL);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(3);
+            message.params[0].should.equal("GameFreak");
+            message.params[1].should.equal("NornAlbion");
+            message.params[2].should.equal("Hello!");
+
+            // directed at multiple
+            var message = new Message("TGameFreak\tNornAlbion\tBlossom\tHello!");
+            message.command.should.equal(constants.MAIL);
+            should.not.exist(message.subcommand);
+            should.not.exist(message.subsubcommand);
+
+            message.params.length.should.equal(4);
+            message.params[0].should.equal("GameFreak");
+            message.params[1].should.equal("NornAlbion");
+            message.params[2].should.equal("Blossom");
+            message.params[3].should.equal("Hello!");
+        });
+
+    });
+
+    describe('That I Don\'t Know The Purpose Of', function() {
 
     });
 
